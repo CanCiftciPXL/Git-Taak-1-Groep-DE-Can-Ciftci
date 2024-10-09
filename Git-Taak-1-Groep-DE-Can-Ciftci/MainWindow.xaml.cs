c5b5bdc06cfc01076d7b61a3868138fb6a31619f
CanCiftciPXL <Can.Ciftci@student.pxl.be>
2024-10-09 11:49:36 +0200

    MOVIE-05

--- Git-Taak-1-Groep-DE-Can-Ciftci/MainWindow.xaml.cs
+++ Git-Taak-1-Groep-DE-Can-Ciftci/MainWindow.xaml.cs
@@ -20,5 +20,29 @@ namespace Git_Taak_1_Groep_DE_Can_Ciftci
         {
             InitializeComponent();
         }
+
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2 && e.ChangedButton == MouseButton.Left)
+            {
+                txtFilm.Text = "Dune: Part Two\r\nPaul Atreides unites with Chani and the Fremen while seeking revenge against the conspirators who destroyed his family.";
+            }
+        }
+
+        private void Image_MouseDown_1(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2 && e.ChangedButton == MouseButton.Left)
+            {
+                txtFilm.Text = "Furiosa: A Mad Max Saga\r\nThe origin story of renegade warrior Furiosa before her encounter and teamup with Mad Max.";
+            }
+        }
+
+        private void Image_MouseDown_2(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2 && e.ChangedButton == MouseButton.Left)
+            {
+                txtFilm.Text = "The Substance\r\nA fading celebrity decides to use a black-market drug, a cell-replicating substance that temporarily creates a younger, better version of herself.";
+            }
+        }
     }
 }
