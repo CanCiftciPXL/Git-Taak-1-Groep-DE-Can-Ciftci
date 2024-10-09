b462a0b690b2762588fb27ad34c96ff78fb998f0
CanCiftciPXL <Can.Ciftci@student.pxl.be>
2024-10-09 10:55:13 +0200

    Add project files.

---
+++ Git-Taak-1-Groep-DE-Can-Ciftci/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Git_Taak_1_Groep_DE_Can_Ciftci
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
